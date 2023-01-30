574d932886239363b1b4d49b12e24ed876eb208b
Evgenii <evgeniikatunkin@gmail.com>
2023-01-30 21:04:49 +0100

    Initial commit. Exercise is ready to check

---
+++ test_items.py
@@ -0,0 +1,18 @@
+import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_item(browser):
+    browser.get(link)
+    # time.sleep(10)
+
+    try:
+        button = browser.find_element(By.CLASS_NAME, "btn-primary")
+        flag = True
+    except NoSuchElementException:
+        flag = False
+
+    assert flag, f"There is no button on the page {link}"
